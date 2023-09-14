f9782100a91fc7b5279b0a20c2ae6101f146904d
MonikaChakraborty <mchakraborty7516@gmail.com>
2023-09-14 14:58:01 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,22 @@ import Header from './components/Header/Header'
 function App() {
 
   const [bookmarks, setBookmarks] = useState([]);
+  const [readingTime, setReadingTime] = useState(0);
 
   const handleAddToBookmark = blog => {
     const newBookmarks = [...bookmarks, blog];
     setBookmarks(newBookmarks);
+  }
+
+  const handleMarkAsRead = (id, time) => {
+    const newReadingTime = readingTime + time;
+    setReadingTime(newReadingTime);
+
+    // remove the read blog from bookmark
+    // console.log('removing', id);
+    const remainingBookmarks = bookmarks.filter(bookmark => bookmark.id !== id);
+    setBookmarks(remainingBookmarks);
+
   }
   return (
     <>
@@ -18,8 +30,8 @@ function App() {
       <Header></Header>
       <div className='md:flex max-w-7xl mx-auto'>
         
-        <Blogs handleAddToBookmark={handleAddToBookmark}></Blogs>
-        <Bookmarks bookmarks={bookmarks}></Bookmarks>
+        <Blogs handleAddToBookmark={handleAddToBookmark} handleMarkAsRead={handleMarkAsRead}></Blogs>
+        <Bookmarks bookmarks={bookmarks} readingTime={readingTime}></Bookmarks>
       </div>
     </>
   )
