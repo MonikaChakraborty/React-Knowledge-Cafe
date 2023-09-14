5fb72221675523248878bef9df57d6e026dd038c
MonikaChakraborty <mchakraborty7516@gmail.com>
2023-09-14 13:56:07 +0600

    Blogs added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 import Blogs from './components/Blogs/Blogs'
 import Bookmarks from './components/Bookmarks/Bookmarks'
@@ -5,14 +6,20 @@ import Header from './components/Header/Header'
 
 function App() {
 
+  const [bookmarks, setBookmarks] = useState([]);
+
+  const handleAddToBookmark = blog => {
+    const newBookmarks = [...bookmarks, blog];
+    setBookmarks(newBookmarks);
+  }
   return (
     <>
       
       <Header></Header>
-      <div className='md:flex'>
+      <div className='md:flex max-w-7xl mx-auto'>
         
-        <Blogs></Blogs>
-        <Bookmarks></Bookmarks>
+        <Blogs handleAddToBookmark={handleAddToBookmark}></Blogs>
+        <Bookmarks bookmarks={bookmarks}></Bookmarks>
       </div>
     </>
   )
